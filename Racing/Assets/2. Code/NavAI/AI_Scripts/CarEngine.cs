24d64fe12ceee8dd79ab3af52b144083ca9f70b9
jjuni10 <annijuni10@naver.com>
2024-11-26 12:17:47 +0900

    [2024-11-26] 브레이크 및 장애물 감지

--- Racing/Assets/2. Code/NavAI/AI_Scripts/CarEngine.cs
+++ Racing/Assets/2. Code/NavAI/AI_Scripts/CarEngine.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class CarEngine : MonoBehaviour
 {
     public Transform path;
-    public float maxSteerAngle = 45f;
+    public float maxSteerAngle = 45f;  // 바퀴의 최대 회전 각도
     public WheelCollider wheelFL;
     public WheelCollider wheelFR;
-    public float maxMotorTorque = 80f;
-    public float currentSpeed;
-    public float maxSpeed = 100f;
+    public WheelCollider wheelRL;
+    public WheelCollider wheelRR;
 
-    public Vector3 centerOfMass;
+    public float maxMotorTorque = 0f;  // 바퀴의 회전력
+    public float maxBrakeTorque = 150f; // 브레이크 되는 값
+    public float currentSpeed;          // 현재 속도
+    public float maxSpeed = 100f;       // 최고 속도
+
+    public Vector3 centerOfMass;        // 자동차의 중점
+
+    public bool isBraking = false;      // 브레이크 상태 체크
+
+    [Header("Sensors")]
+    public float sensorLength = 15f;
+    public Vector3 frontSensorPosition = new Vector3(0, 0.65f, 0.5f);
+    public float frontSideSensorPosition = 1f;
+    public float frontSensorAngle = 30f;
 
     private List<Transform> nodes;
     private int currentNode = 0;
@@ -43,9 +55,48 @@ public class CarEngine : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
+        Sensors();
         ApplySteer();
         Drive();
         CheckWaypointDistance();
+        Braking();
+    }
+    private void Sensors()
+    {
+        RaycastHit hit;
+        Vector3 sensorStartPos = transform.position + frontSensorPosition;
+
+        // front center 센서
+        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
+        {
+            Debug.DrawLine(sensorStartPos, hit.point);
+        }
+
+        // front right 센서
+        sensorStartPos.x += frontSideSensorPosition;
+        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
+        {
+            Debug.DrawLine(sensorStartPos, hit.point);
+        }
+
+        // front right angle 센서
+        if (Physics.Raycast(sensorStartPos, Quaternion.AngleAxis(frontSensorAngle, transform.up) * transform.forward, out hit, sensorLength))
+        {
+            Debug.DrawLine(sensorStartPos, hit.point);
+        }
+
+        // front left 센서
+        sensorStartPos.x -= 2 * frontSideSensorPosition;
+        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
+        {
+            Debug.DrawLine(sensorStartPos, hit.point);
+        }
+
+        // front left angle 센서
+        if (Physics.Raycast(sensorStartPos, Quaternion.AngleAxis(-frontSensorAngle, transform.up) * transform.forward, out hit, sensorLength))
+        {
+            Debug.DrawLine(sensorStartPos, hit.point);
+        }
     }
 
     private void ApplySteer()
@@ -60,7 +111,7 @@ public class CarEngine : MonoBehaviour
     {
         currentSpeed = 2 * Mathf.PI * wheelFL.radius * wheelFL.rpm * 60 / 1000;
 
-        if(currentSpeed < maxSpeed)
+        if(currentSpeed < maxSpeed && !isBraking)
         {
             wheelFL.motorTorque = maxMotorTorque;
             wheelFR.motorTorque = maxMotorTorque;
@@ -73,7 +124,7 @@ public class CarEngine : MonoBehaviour
     }
     private void CheckWaypointDistance()
     {
-        if(Vector3.Distance(transform.position, nodes[currentNode].position) < 5.0f)
+        if(Vector3.Distance(transform.position, nodes[currentNode].position) < 10.0f)
         {
             if(currentNode == nodes.Count - 1)
             {
@@ -83,6 +134,19 @@ public class CarEngine : MonoBehaviour
             {
                 currentNode++;
             }
+        }
+    }
+    private void Braking()
+    {
+        if (isBraking)
+        {
+            wheelRL.brakeTorque = maxBrakeTorque;
+            wheelRR.brakeTorque = maxBrakeTorque;
+        }
+        else
+        {
+            wheelRL.brakeTorque = 0;
+            wheelRR.brakeTorque = 0;
         }
     }
 }
